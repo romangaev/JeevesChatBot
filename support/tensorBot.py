423cfa4d5cc912a9b37c53119631f316e86219fc
romangaev <rgnok@mail.ru>
2018-08-02 00:42:20 +0100

    trying to classify the intent

--- support/tensorBot.py
+++ support/tensorBot.py
@@ -10,89 +10,6 @@ import tensorflow as tf
 import random
 from nltk.corpus import stopwords
 
-# import our chat-bot intents file
-import json
-with open('intents.json') as json_data:
-    intents = json.load(json_data)
-words = []
-classes = []
-documents = []
-ignore_words = ['?']
-# loop through each sentence in our intents patterns
-for intent in intents['intents']:
-    for pattern in intent['patterns']:
-        # tokenize each word in the sentence
-        w = nltk.word_tokenize(pattern)
-        # add to our words list
-        words.extend(w)
-        # add to documents in our corpus
-        documents.append((w, intent['tag']))
-        # add to our classes list
-        if intent['tag'] not in classes:
-            classes.append(intent['tag'])
-
-# stem and lower each word and remove duplicates
-words = [stemmer.stem(w.lower()) for w in words if w not in ignore_words]
-words = sorted(list(set(words)))
-
-# remove duplicates
-classes = sorted(list(set(classes)))
-
-
-# remove stop words
-words = [w for w in words if w not in stopwords.words('english')]
-words = [w for w in words if w not in stopwords.words('russian')]
-
-print (len(documents), "documents")
-print (len(classes), "classes", classes)
-print (len(words), "unique stemmed words", words)
-
-# create our training data
-training = []
-output = []
-# create an empty array for our output
-output_empty = [0] * len(classes)
-
-# training set, bag of words for each sentence
-for doc in documents:
-    # initialize our bag of words
-    bag = []
-    # list of tokenized words for the pattern
-    pattern_words = doc[0]
-    # stem each word
-    pattern_words = [stemmer.stem(word.lower()) for word in pattern_words]
-    # create our bag of words array
-    for w in words:
-        bag.append(1) if w in pattern_words else bag.append(0)
-
-    # output is a '0' for each tag and '1' for current tag
-    output_row = list(output_empty)
-    output_row[classes.index(doc[1])] = 1
-
-    training.append([bag, output_row])
-
-# shuffle our features and turn into np.array
-random.shuffle(training)
-training = np.array(training)
-
-# create train and test lists
-train_x = list(training[:,0])
-train_y = list(training[:,1])
-
-# reset underlying graph data
-tf.reset_default_graph()
-# Build neural network
-net = tflearn.input_data(shape=[None, len(train_x[0])])
-net = tflearn.fully_connected(net, 8)
-net = tflearn.fully_connected(net, 8)
-net = tflearn.fully_connected(net, len(train_y[0]), activation='softmax')
-net = tflearn.regression(net)
-
-# Define model and setup tensorboard
-model = tflearn.DNN(net, tensorboard_dir='tflearn_logs')
-# Start training (apply gradient descent algorithm)
-model.fit(train_x, train_y, n_epoch=1000, batch_size=8, show_metric=True)
-model.save('model.tflearn')
 
 def clean_up_sentence(sentence):
     # tokenize the pattern
@@ -118,6 +35,87 @@ def bow(sentence, words, show_details=False):
 
 ERROR_THRESHOLD = 0.25
 def classify(sentence):
+    # import our chat-bot intents file
+    import json
+    with open('intents.json') as json_data:
+        intents = json.load(json_data)
+    words = []
+    classes = []
+    documents = []
+    ignore_words = ['?']
+    # loop through each sentence in our intents patterns
+    for intent in intents['intents']:
+        for pattern in intent['patterns']:
+            # tokenize each word in the sentence
+            w = nltk.word_tokenize(pattern)
+            # add to our words list
+            words.extend(w)
+            # add to documents in our corpus
+            documents.append((w, intent['tag']))
+            # add to our classes list
+            if intent['tag'] not in classes:
+                classes.append(intent['tag'])
+
+    # stem and lower each word and remove duplicates
+    words = [stemmer.stem(w.lower()) for w in words if w not in ignore_words]
+    words = sorted(list(set(words)))
+
+    # remove duplicates
+    classes = sorted(list(set(classes)))
+
+    # remove stop words
+    words = [w for w in words if w not in stopwords.words('english')]
+    words = [w for w in words if w not in stopwords.words('russian')]
+
+    print(len(documents), "documents")
+    print(len(classes), "classes", classes)
+    print(len(words), "unique stemmed words", words)
+
+    # create our training data
+    training = []
+    output = []
+    # create an empty array for our output
+    output_empty = [0] * len(classes)
+
+    # training set, bag of words for each sentence
+    for doc in documents:
+        # initialize our bag of words
+        bag = []
+        # list of tokenized words for the pattern
+        pattern_words = doc[0]
+        # stem each word
+        pattern_words = [stemmer.stem(word.lower()) for word in pattern_words]
+        # create our bag of words array
+        for w in words:
+            bag.append(1) if w in pattern_words else bag.append(0)
+
+        # output is a '0' for each tag and '1' for current tag
+        output_row = list(output_empty)
+        output_row[classes.index(doc[1])] = 1
+
+        training.append([bag, output_row])
+
+    # shuffle our features and turn into np.array
+    random.shuffle(training)
+    training = np.array(training)
+
+    # create train and test lists
+    train_x = list(training[:, 0])
+    train_y = list(training[:, 1])
+
+    # reset underlying graph data
+    tf.reset_default_graph()
+    # Build neural network
+    net = tflearn.input_data(shape=[None, len(train_x[0])])
+    net = tflearn.fully_connected(net, 8)
+    net = tflearn.fully_connected(net, 8)
+    net = tflearn.fully_connected(net, len(train_y[0]), activation='softmax')
+    net = tflearn.regression(net)
+
+    # Define model and setup tensorboard
+    model = tflearn.DNN(net, tensorboard_dir='tflearn_logs')
+
+    model.load('model.tflearn')
     # generate probabilities from the model
     results = model.predict([bow(sentence, words)])[0]
     # filter out predictions below a threshold
@@ -129,8 +127,7 @@ def classify(sentence):
         return_list.append((classes[r[0]], r[1]))
     # return tuple of intent and probability
     return return_list
-
-def response(sentence, userID='123', show_details=False):
+'''def response(sentence, userID='123', show_details=False):
     results = classify(sentence)
     # if we have a classification then find the matching intent tag
     if results:
@@ -142,5 +139,7 @@ def response(sentence, userID='123', show_details=False):
                     # a random response from the intent
                     return print(random.choice(i['responses']))
 
-            results.pop(0)
+            results.pop(0)'''
 
+
+print(classify('hello'))
