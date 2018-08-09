65ccf0824684052c9d3c8c6f1b7306b8d8422a5a
romangaev <rgnok@mail.ru>
2018-08-09 02:09:21 +0100

    corrected state machine module object

--- StateMachine.py
+++ StateMachine.py
@@ -10,11 +10,14 @@ class StateMachine:
     def state_respond(self,intent,confidence,new_state):
         with open('intents.json') as json_data:
             intents = json.load(json_data)
-
+        response='didnt get ya'
         if confidence<0.3:
-            return "Didnt get ya"
+            return response
         elif intent == 'dictopen':
             response = random.choice(intents['intents'][4]['responses'])
+            #DB QUERY
+        elif intent == 'dictadd':
+            response = random.choice(intents['intents'][5]['responses'])
             #DB QUERY
         elif intent == 'info':
             response=random.choice(intents['intents'][1]['responses'])
