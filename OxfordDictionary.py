5cb874c278c46e2317df5a679e3d65d7c7ca5ebf
romangaev <rgnok@mail.ru>
2018-08-14 17:34:12 +0100

    adding Oxford dictionary

--- OxfordDictionary.py
+++ OxfordDictionary.py
@@ -14,34 +14,37 @@ def oxford_dic_request(word_id):
     # print("code {}\n".format(r.status_code))
     print("text \n" + r.text)
     # print("json \n" + json.dumps(r.json()))
-    oxford_dict = r.json()
-    response = ""
-    audio_url = ""
-    for i in oxford_dict["results"]:
-        # print(word_id)
-        # print(i["lexicalEntries"][0]["pronunciations"][0]["phoneticSpelling"])
-        response += word_id
-        response += '\n'
-        response += i["lexicalEntries"][0]["pronunciations"][0]["phoneticSpelling"]
-        def_counter = 1
-        for j in i["lexicalEntries"]:
-            # print(j["lexicalCategory"])
-            response += '\n\n'
-            response +=str(def_counter)+"."+ j["lexicalCategory"]
-            def_counter += 1
-            if "audioFile" in j["pronunciations"][0]:
-                audio_url = j["pronunciations"][0]["audioFile"]
-            for k in j["entries"]:
+    if r is not None:
+        oxford_dict = r.json()
+        response = ""
+        audio_url = ""
+        for i in oxford_dict["results"]:
+            # print(word_id)
+            # print(i["lexicalEntries"][0]["pronunciations"][0]["phoneticSpelling"])
+            response += word_id
+            response += '\n'
+            response += i["lexicalEntries"][0]["pronunciations"][0]["phoneticSpelling"]
+            def_counter = 1
+            for j in i["lexicalEntries"]:
+                # print(j["lexicalCategory"])
+                response += '\n\n'
+                response +=str(def_counter)+"."+ j["lexicalCategory"]
+                def_counter += 1
+                if "audioFile" in j["pronunciations"][0]:
+                    audio_url = j["pronunciations"][0]["audioFile"]
+                for k in j["entries"]:
 
-                for v in k["senses"]:
-                    for w in v["definitions"]:
-                        # print("\t "+str(def_counter)+": "+w)
-                        response += '\n'
-                        response += "- "+w
+                    for v in k["senses"]:
+                        for w in v["definitions"]:
+                            # print("\t "+str(def_counter)+": "+w)
+                            response += '\n'
+                            response += "- "+w
 
-                        if "examples" in v:
-                            for s in v["examples"]:
-                            #    print('\t\tExample: '+str(w["text"]))
-                                response += '\n'
-                                response += 'Example: '+str(s["text"])
-    return {"text": response, "attachment": audio_url}
+                            if "examples" in v:
+                                for s in v["examples"]:
+                                #    print('\t\tExample: '+str(w["text"]))
+                                    response += '\n'
+                                    response += 'Example: '+str(s["text"])
+        return {"text": response, "attachment": audio_url}
+    else:
+        return {"text": "I don't know this word", "attachment": None}
