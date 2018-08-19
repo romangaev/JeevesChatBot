ce0529e0abb16a33b7fdc11b5497942ca4828785
romangaev <rgnok@mail.ru>
2018-08-19 20:10:58 +0100

    WORD OF THE DAY

--- bin/send_all_subscription.py
+++ bin/send_all_subscription.py
@@ -1,6 +1,10 @@
 import os
+import random
+
 from pymessenger.bot import Bot
 from pymongo import MongoClient
+
+import OxfordDictionary
 from subscriptions import get_podcasts
 
 MONGODB_URI = os.environ['MONGODB_URI']
@@ -8,10 +12,13 @@ ACCESS_TOKEN = os.environ['ACCESS_TOKEN']
 client = MongoClient(MONGODB_URI)
 db = client.chatbot_db
 user_subscriptions_collection = db.user_subscriptions_collection
-podcasts_feed_collection = db.user_podcasts_feed_collection_collection
+user_state_collection=db.user_state_collection
+podcasts_feed_collection = db.podcasts_feed_collection
+phrase_of_the_day_collection = db.phrase_of_the_day_collection
 bot = Bot(ACCESS_TOKEN)
 all_tags=["bbc",'football','politics','science','longreads','technology','global']
 
+# SEND ALL SUBSCRIPTIONS
 for every in all_tags:
     latest = get_podcasts(every)[0]['title']
     if not latest==podcasts_feed_collection.posts.find_one({'tag': every})['latest']:
@@ -68,3 +75,40 @@ for every in all_tags:
                                 'REGULAR'
                         }
                         bot.send_raw(payload)
+
+# SEND THE PHRASE OF THE DAY
+for document in user_state_collection.posts.find():
+    user_id = document["user_id"]
+    dic={"text": "I don't know this word", "attachment": None, "examples": None}
+    while not dic["text"]=="I don't know this word":
+        types=['idioms','phrasal_verbs']
+        type=random.choice(types)
+        result = phrase_of_the_day_collection.posts.find_one({'type': type})
+        phrase = result["phrase"][result["current_number"]]
+        # update number
+        phrase_of_the_day_collection.posts.update_one({'type': type}, {"$inc": {'current_number': +1}}, upsert=True)
+
+        dic = OxfordDictionary.oxford_dic_request(phrase)
+    text = dic["text"]
+    elements = []
+    elements.append({
+                    "title":dic["text"].split("\n",1)[0],
+                    "image_url": 'https://is1-ssl.mzstatic.com/image/thumb/Purple118/v4/2c/96/13/2c9613fb-cbbf-3910-15fd-92540ff89a16/mzl.ytnapyct.png/246x0w.jpg',
+                    "subtitle": dic["text"].split("\n",1)[1],
+                    "default_action": {
+                        "type": "web_url",
+                        "url": "",
+                        "webview_height_ratio": "tall",
+                    },
+                    "buttons":[{"type":"postback",
+                                            "title":"Examples",
+                                            "payload":"OXFORD_DIC_EXAMPLES"},
+                                          {"type": "postback",
+                                            "title": "Synonyms-Antonyms",
+                                            "payload": "OXFORD_DIC_SYNONYMS"},
+                                          {"type": "postback",
+                                                    "title": "Pronunciation",
+                                                    "payload": "OXFORD_DIC_PRONUNCIATION"}
+                                          ]
+                })
+    bot.send_generic_message(user_id, elements)
