a74129d8deb7b25a1644ed39e264b7ebbf28a640
romangaev <rgnok@mail.ru>
2018-08-19 02:43:33 +0100

    added cron job

--- bin/send_all_subscription.py
+++ bin/send_all_subscription.py
@@ -8,52 +8,62 @@ ACCESS_TOKEN = os.environ['ACCESS_TOKEN']
 client = MongoClient(MONGODB_URI)
 db = client.chatbot_db
 user_subscriptions_collection = db.user_subscriptions_collection
+podcasts_feed_collection = db.user_podcasts_feed_collection_collection
 bot = Bot(ACCESS_TOKEN)
+all_tags=["bbc",'football','politics','science','longreads','technology','global']
 
-for document in user_subscriptions_collection.posts.find():
-    user_id = document['user_id']
-    tags = document['tags']
-    for tag in tags:
-        podcasts = get_podcasts(tag)
-        elements = []
-        elements.append({
-            "title": podcasts[0]['title'],
-            "image_url": podcasts[0]['img'],
-            "subtitle": podcasts[0]['description'],
-            "default_action": {
-                "type": "web_url",
-                "url": podcasts[0]['link'],
-                "webview_height_ratio": "tall",
-            },
-            "buttons": [
-                {
-                    "type": "web_url",
-                    "url": podcasts[0]['link'],
-                    "title": "Listen!"
-                }, {
-                    "type": "postback",
-                    "title": 'UNSUBSCRIBE',
-                    "payload": 'UNSUBSCRIBE' + "_" + tag
-                }
-            ]
-        })
+for every in all_tags:
+    latest = get_podcasts(every)[0]['title']
+    if not latest==podcasts_feed_collection.posts.find_one({'tag': 'bbc'}):
+            podcasts_feed_collection.posts.update_one({'tag': 'bbc'},{"$set": {'latest':latest}}, upsert=True)
 
-        payload = {
-            'message': {
-                "attachment": {
-                    "type": "template",
-                    "payload": {
-                        "template_type": "generic",
-                        "elements": elements
-                    }
-                }
-            },
-            "tag":
-                "NON_PROMOTIONAL_SUBSCRIPTION",
-            'recipient':{
-                'id': user_id
-            },
-            'notification_type':
-                'REGULAR'
-        }
-        bot.send_raw(payload)
+            for document in user_subscriptions_collection.posts.find():
+                user_id = document['user_id']
+                tags = document['tags']
+                bot.send_text_message(user_id,
+                                      "Hey! Here are some fresh stuff for you!\nDon't be lazy and improve your english every day!")
+                for tag in tags:
+                    if tag == every:
+                        podcasts = get_podcasts(tag)
+                        elements = []
+                        elements.append({
+                            "title": podcasts[0]['title'],
+                            "image_url": podcasts[0]['img'],
+                            "subtitle": podcasts[0]['description'],
+                            "default_action": {
+                                "type": "web_url",
+                                "url": podcasts[0]['link'],
+                                "webview_height_ratio": "tall",
+                            },
+                            "buttons": [
+                                {
+                                    "type": "web_url",
+                                    "url": podcasts[0]['link'],
+                                    "title": "Listen!"
+                                }, {
+                                    "type": "postback",
+                                    "title": 'UNSUBSCRIBE',
+                                    "payload": 'UNSUBSCRIBE' + "_" + tag
+                                }
+                            ]
+                        })
+
+                        payload = {
+                            'message': {
+                                "attachment": {
+                                    "type": "template",
+                                    "payload": {
+                                        "template_type": "generic",
+                                        "elements": elements
+                                    }
+                                }
+                            },
+                            "tag":
+                                "NON_PROMOTIONAL_SUBSCRIPTION",
+                            'recipient': {
+                                'id': user_id
+                            },
+                            'notification_type':
+                                'REGULAR'
+                        }
+                        bot.send_raw(payload)
