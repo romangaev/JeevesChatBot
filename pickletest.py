a27c3fff29b911e6c2ce50067f489971b0dc6fd1
romangaev <rgnok@mail.ru>
2018-08-20 01:08:03 +0100

    WORD OF THE DAY

--- pickletest.py
+++ pickletest.py
@@ -1,9 +1,13 @@
-from pymongo import MongoClient
+from random import shuffle
 
+from pymongo import MongoClient
+'''
 MONGODB_URI ="mongodb://rgaev:iha492081@ds115592.mlab.com:15592/chatbot_db"
 client = MongoClient(MONGODB_URI)
 db = client.chatbot_db
 phrase_of_the_day_collection = db.phrase_of_the_day_collection
 
-phrase_of_the_day_collection.posts.update_one({'type': 'phrasal_verbs'}, {"$set": {'current_number': 0}}, upsert=True)
-phrase_of_the_day_collection.posts.update_one({'type': 'idioms'}, {"$set": {'current_number': 0}}, upsert=True)
+dic =phrase_of_the_day_collection.posts.find_one({"type":"idioms"})
+list=dic["phrase"]
+shuffle(list)
+phrase_of_the_day_collection.posts.update_one({"type":"idioms"},{ '$set':{"phrase":list}})'''
